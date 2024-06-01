118bcd8214bb5fe444f9608bbe76fd9fc52daf83
Hassan Ali <hassankasuri0@gmail.com>
2024-06-01 17:09:34 +0500

    Moved time logs in dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/timelogs', [TimeLogController::class, 'index'])->name('timelogs.index');
+    Route::get('/dashboard', [TimeLogController::class, 'index'])->name('dashboard');
     Route::get('/employees', [EmployeeController::class, 'index'])->name('employees.index');
 });
 
